2e3de19018bcb5b623df4fb53794c1a692ee48d1
Gastalver <mgastalver@gmail.com>
2017-03-14 01:06:31 +0100

    Mi primera app ng2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ChisteComponent } from './chiste/chiste.component';
-import { ListachistesComponent } from './listachistes/listachistes.component';
 import { UserItemComponent } from './user-item/user-item.component';
 import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChisteComponent,
-    ListachistesComponent,
     UserItemComponent,
     UserListComponent
   ],
